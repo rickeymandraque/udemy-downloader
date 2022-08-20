36f40211d8233bf3cc1ea3d40afcc3799fd6e347
rickeymandraque <rickeymandraque@users.noreply.github.com>
2022-08-20 09:16:30 +0200

    Create Install.sh

---
+++ Bash_Script/Install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Warning Not tested ! This script is for my personal configuration. DO NOT USE !
+echo "Warning Not tested ! This script is for my personal configuration. DO NOT USE ! USE CTL+C"
+
+sudo apt -s install python3-mpegdash python3-tqdm python3-requests python3-dotenv python3-protobuf python3-webvtt python3-pysrt python3-m3u8 python3-colorama python3-bitstring python3-unidecode python3-bs4 python3-lxml python3-six python3-pathvalidate python3-coloredlogs python3-xmltodict atom discord firefox firefox-locale-fr mkvtoolnix yarn youtube-dl yt-dlp python3 build-essential manpages-fr manpages-fr-extra tldr wget curl aria2 libssl-dev gcc automake screen zlib1g-dev libjpeg-dev mint-meta-codecs vlc celluloid ffmpeg p7zip-rar catfish p7zip-full fonts-crosextra-carlito fonts-crosextra-caladea mkvtoolnix-gui gdebi
+sudo pip install cloudscraper
