d0e1c8c1a5a8da696a39db8cce9333658fcfc665
rickeymandraque <rickeymandraque@users.noreply.github.com>
2022-08-20 09:22:13 +0200

    remove "name" option

--- Bash_Script/cookie.sh
+++ Bash_Script/cookie.sh
@@ -2,10 +2,9 @@
 
 function export_cookie() {
 reponse="$(sqlite3 $HOME/.mozilla/firefox/*.default-release/cookies.sqlite <<EOF
-SELECT name,value FROM moz_cookies WHERE host="www.udemy.com" and name LIKE "%%access_token%%";
+SELECT value FROM moz_cookies WHERE host="www.udemy.com" and name LIKE "%%access_token%%";
 EOF
 )"
-cookie="${reponse##*\|}"
-echo $cookie
+echo $reponse
 }
 export_cookie
