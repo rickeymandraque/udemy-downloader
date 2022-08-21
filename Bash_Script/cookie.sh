be6240a9c2162179306492f7ec1b37a5965316af
rickeymandraque <rickeymandraque@users.noreply.github.com>
2022-08-21 08:10:33 +0200

    add ideas
    add variable for next functions.
    To-Do :
    Wget to firefox and extract
    Open firefox in popup-mode
    monitor the process and wait for it to complete.
    export cookie !

--- Bash_Script/cookie.sh
+++ Bash_Script/cookie.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+echo "This script work only with firefox"
+echo "This script not work yet"
+
+Get-Locale="$(locale | grep -E '^LANG=' | awk -F= '{print $2}'| awk -F. '{print $1}')"
+Minus_Local="$(Get-Local | awk -F_ '{print $1}')"
+Up_Local="$(Get-Local | awk -F_ '{print $2}')"
+
+
+if [[ $Minus_Locale = "fr" ]]; then
+  Locale="fr"
+else
+  Locale="$Minus_Locale-$Up_Local"
+fi
+
+
 function export_cookie() {
 reponse="$(sqlite3 $HOME/.mozilla/firefox/*.default-release/cookies.sqlite <<EOF
 SELECT value FROM moz_cookies WHERE host="www.udemy.com" and name LIKE "%%access_token%%";
@@ -7,4 +22,19 @@ EOF
 )"
 echo $reponse
 }
+
+# 21/08/2022
+# New idea ! Download firefox portable mode for linux.
+# open it with the good url via bash, just 1 cookie.
+# same thing with chrome.
+# url = https://www.udemy.com/join/login-popup/?locale=${Get-Locale}&response_type=html&next=https%3A%2F%2Fwww.udemy.com%2F
+# need to get local from computer (locale | grep -E ^LANG=)
+# need confirmation for local
+# need to find a solution in bash to get cookie for chrome.
+# the firefox portable solution don't need chrome !!! :)
+# Firefox portable https://gist.github.com/thewh1teagle/6a739c959b613f839eb3c5f9895a4505
+# Firefox latest link https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=${Locale}
+# French is fr; english us is en-US ...
+# need list of local (apt-cache output > list.lst ? )
+
 export_cookie
