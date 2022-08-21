0633a50e265e7959f305f6fd5000d6bad0fb76d2
rickeymandraque <rickeymandraque@users.noreply.github.com>
2022-08-21 07:36:25 +0200

    Create Cookie.ps1
    just many ideas

---
+++ PS_Scripts/Cookie.ps1
@@ -0,0 +1,7 @@
+sqlite3 = "https://www.sqlite.org/2022/sqlite-dll-win64-x64-3390200.zip"
+Get-cookie = "SELECT value FROM moz_cookies WHERE host="www.udemy.com" and name LIKE "%%access_token%%";"
+Nirsoft = "https://www.nirsoft.net/utils/chromecookiesview.zip"
+Chrome_Portable = "https://portableapps.com/redir2/?a=GoogleChromePortable&s=s&p=&d=sf&f=GoogleChromePortable_104.0.5112.102_online.paf.exe"
+Firefox_Portable = "https://portableapps.com/downloading/?a=FirefoxPortable&s=s&p=&d=pa&f=FirefoxPortable_103.0.2_English.paf.exe"
+
+write-host " not implemented for now"
