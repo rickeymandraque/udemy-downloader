d1e03b40656b20ca0f63cda61d1268378ed2f700
rickeymandraque <rickeymandraque@users.noreply.github.com>
2022-08-22 19:01:11 +0200

    Update Install.sh

--- Bash_Script/Install.sh
+++ Bash_Script/Install.sh
@@ -18,6 +18,7 @@ function mkcd() {
 
 mkcd $Download_Dir
 
+gpgconf --kill dirmngr
 
 sudo -E gpg --lock-never --no-default-keyring --keyring=/usr/share/keyrings/javinator9889-ppa-keyring.gpg --keyserver keyserver.ubuntu.com --recv-keys 08633B4AAAEB49FC
 sudo tee /etc/apt/sources.list.d/javinator9889-ppa.list <<< "deb [arch=amd64] https://ppa.javinator9889.com all main"
